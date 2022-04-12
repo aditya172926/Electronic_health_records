31c4047ee8caa415ccee478ebbc666ccbf6e5ef3
Aditya <aditya26sg@gmail.com>
2022-04-12 18:02:29 +0530

    added bootstrap files

--- mp_project/mp_project/urls.py
+++ mp_project/mp_project/urls.py
@@ -1,8 +1,6 @@
 
 from django.contrib import admin
 from django.urls import path, include
-# from logapp.views import index
-from auapp.views import user_signup, user_login, user_logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
