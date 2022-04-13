97620811fbdfe2506459358d0d2cab9ad499bb51
Aditya <aditya26sg@gmail.com>
2022-04-13 11:18:05 +0530

    added JS to connect wallet

--- mp_project/logapp/static/logapp/js/logapp.js
+++ mp_project/logapp/static/logapp/js/logapp.js
@@ -2,7 +2,34 @@ const test2 = () => {
     console.log("Function working");
 }
 
+var currentAccount;
+
 function test() {
     console.log("Function working");
-    console.log(ethereum);
+    try {
+        const ethereum = window;
+        if (!ethereum) {
+            console.log("Make sure you have metamask");
+            return;
+        } else {
+            console.log("We have ethereum object ", ethereum);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const connectWallet = async () => {
+    try {
+        const {ethereum} = window;
+        if (!ethereum) {
+            alert("Get Metamask extension!");
+            return;
+        }
+        const accounts = await ethereum.request({method:"eth_requestAccounts"});
+        console.log("Connected", accounts[0]);
+        currentAccount = accounts[0];
+    } catch (error) {
+        console.log(error);
+    }
 }
