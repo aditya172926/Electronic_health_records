dd0526e9a2c22dab53189a29e40c955e882edf1f
Aditya <aditya26sg@gmail.com>
2022-04-21 11:05:07 +0530

    added models

--- mp_project/logapp/admin.py
+++ mp_project/logapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Patient, Doctor, Appointments
+
+admin.site.site_header = "Electronic Health Records"
 
 # Register your models here.
--- mp_project/logapp/models.py
+++ mp_project/logapp/models.py
@@ -1,3 +1,25 @@
+from xml.etree.ElementInclude import default_loader
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    patient_wallet_address = models.CharField(max_length=200, default="")
+    patient_name = models.CharField(max_length=200, default="")
+    patient_resi_address = models.CharField(max_length=200, default="")
+    patient_contact_number = models.CharField(max_length=200, default="")
+
+    def __str__(self):
+        return self.patient_name
+
+class Doctor(models.Model):
+    doctor_wallet_address = models.CharField(max_length=200, default="")
+    doctor_name = models.CharField(max_length=200, default="")
+    doctor_resi_address = models.CharField(max_length=200, default="")
+    doctor_contact_number = models.CharField(max_length=200, default="")
+
+    def __str__(self):
+        return self.doctor_name
+
+class Appointments(models.Model):
+    appointment_head = models.CharField(max_length=200, default="")
+    appointment_details = models.CharField(max_length=200, default="")
